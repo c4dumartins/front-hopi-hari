ec56d1f480dae04b5dbc4d382018c26abfada6b8
Carlos Santos <57176147809@escola928.edu>
2025-05-26 17:02:08 -0300

    2605

--- pages/dashboard/script.js
+++ pages/dashboard/script.js
@@ -1,19 +1,19 @@
-
         // Verificar se há um usuário logado
         document.addEventListener('DOMContentLoaded', function() {
+            const userToken = JSON.parse(localStorage.getItem('token')) || {};
             const userData = JSON.parse(localStorage.getItem('user')) || {};
             
-            if (userData.firstName) {
+            if (userToken) {
                 document.getElementById('user-name').textContent = `Olá, ${userData.firstName}!`;
                 document.getElementById('user-email').textContent = userData.email;
             } else {
                 // Redirecionar para a página de login se não houver usuário
-                window.location.href = '/';
+                window.location.href = '../login/login.html';
             }
             
             // Botão de logout
             document.getElementById('logout-btn').addEventListener('click', function() {
-                localStorage.removeItem('user');
+                localStorage.removeItem('token');
                 window.location.href = '/';
             });
             
